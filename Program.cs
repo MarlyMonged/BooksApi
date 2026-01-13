ce03ad7dd765961ae0575f7a78de599172903ec6
marly <maro.m5050m@gmail.com>
2026-01-13 14:35:17 +0200

    Configure DbContext

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using BooksApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BooksApi
 {
     public class Program
@@ -12,6 +15,8 @@ namespace BooksApi
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
