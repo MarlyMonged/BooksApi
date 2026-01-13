791b3fbc4974d9d01bf5786d84bf20368fca83c0
marly <maro.m5050m@gmail.com>
2026-01-13 14:29:08 +0200

    Add DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BooksApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksApi.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+            
+        
+       
+    }
+}
