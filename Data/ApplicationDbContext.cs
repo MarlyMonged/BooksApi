81524907c597833c9273e7077454b14e12148717
marly <maro.m5050m@gmail.com>
2026-01-29 22:35:28 +0200

    Add Book and BookAuthors DbSet

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,10 @@ namespace BooksApi.Data
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<BookAuthor>BookAuthors { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
             
         
