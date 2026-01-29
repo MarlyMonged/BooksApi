210a34fe0df01f720b611f135ed68de53eb537b8
marly <maro.m5050m@gmail.com>
2026-01-29 22:39:11 +0200

    Add Fluent Api

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,6 +12,21 @@ namespace BooksApi.Data
 
         public DbSet<BookAuthor>BookAuthors { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(b => b.Book)
+                .WithMany(ba => ba.BookAuthors)
+                .HasForeignKey(bi => bi.BookId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(a => a.Author)
+                .WithMany(ba => ba.BookAuthors)
+                .HasForeignKey(x => x.AuthorId);
+        }
             
         
        
