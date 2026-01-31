fcf5fa30f4f67b0352db56dcc2ca2f59c422cb2c
marly <maro.m5050m@gmail.com>
2026-01-31 21:29:55 +0200

    Add Book Service

---
+++ Services/BookService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using BooksApi.Dtos.Book;
+using BooksApi.Interfaces;
+using BooksApi.Models;
+
+namespace BooksApi.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public BookService(IUnitOfWork unitOfWork, IMapper mapper)
+        {                 
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<int> CreateBookAsync(CreateBookDto dto)
+        {
+            if(dto.AuthorIds == null || !dto.AuthorIds.Any())
+            {
+                throw new ArgumentException("At least one author ID must be provided.", nameof(dto.AuthorIds));
+            }
+
+            var book = _mapper.Map<Book>(dto);
+
+            book.BookAuthors = dto.AuthorIds.Select(authorId => new BookAuthor
+            {
+                AuthorId = authorId
+            }).ToList();
+
+            await _unitOfWork.Books.AddAsync(book);
+            await _unitOfWork.Save();
+
+            return book.Id;
+
+
+        }
+    }
+}
---
+++ Services/IBookService.cs
@@ -0,0 +1,9 @@
+﻿using BooksApi.Dtos.Book;
+
+namespace BooksApi.Services
+{
+    public interface IBookService
+    {
+        Task<int> CreateBookAsync(CreateBookDto dto);
+    }
+}
