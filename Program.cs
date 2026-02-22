92269161949f3b25d5c0beb8b42a9ee4c78225b8
marly <maro.m5050m@gmail.com>
2026-02-22 22:04:14 +0200

    Configuration Jwt in program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@
 using BooksApi.Data;
 using BooksApi.Exceptions;
 using BooksApi.Interfaces;
+using BooksApi.Models;
 using BooksApi.Repositories;
 using BooksApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using System.Text;
 
 namespace BooksApi
 {
@@ -16,9 +21,9 @@ namespace BooksApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(builder.Configuration)
-            .CreateLogger();
+            //Log.Logger = new LoggerConfiguration()
+            //.ReadFrom.Configuration(builder.Configuration)
+            //.CreateLogger();
             builder.Host.UseSerilog();
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -31,6 +36,30 @@ namespace BooksApi
             builder.Services.AddScoped<IAuthorService, AuthorService>();
             builder.Services.AddScoped<IPublisherService, PublisherService>();
             builder.Services.AddAutoMapper(cfg=> { },typeof(Program));
+            builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidAudience = builder.Configuration["JWT:Audience"],
+                        ValidIssuer = builder.Configuration["JWT:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]!))
+                    };
+            });
+
 
             var app = builder.Build();
             //DataSeeder.SeedData(app);
